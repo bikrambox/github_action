3e6e6a86013c2c8a5e9dc845260e7034f3a463a9
Bikram Rongpi <bikramrongpi088@gmail.com>
2024-04-05 14:42:42 +0530

    Create release.config.js

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    "@semantic-release/changelog": "^6.0.3",
+    "@semantic-release/exec": "^6.0.3",
+    "@semantic-release/git": "^10.0.1",
+    "semantic-release": "^23.0.6"
+  ],
+};
